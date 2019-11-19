cd919fdb944835612303ada5f3351ac7594884c9
AisalkynM <53709415+AisalkynM@users.noreply.github.com>
2019-11-19 13:21:43 -0600

    Revert "added urls and views"

--- apps/savour_app/urls.py
+++ apps/savour_app/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns = [
 url(r'^savour/dashboard$', views.savour_dashboard),
-url(r'^savour/recipes$', views.savour_recipes),
-url(r'^savour/favorites$', views.savour_favorites),
-url(r'^savour/list$', views.savour_list),
-url(r'^savour/pantry$', views.savour_pantry)
+# url(r'^trip/new$', views.trip_new),
+# url(r'^trip/create$', views.trip_create),
+# url(r'^trip/(?P<trip_id>\d+)$', views.trip_view),
+# url(r'^trip/edit/(?P<trip_id>\d+)$', views.trip_edit),
+# url(r'^trip/update/(?P<trip_id>\d+)$', views.trip_update),
+# url(r'^trip/remove/(?P<trip_id>\d+)$', views.trip_remove),
+
 ]
